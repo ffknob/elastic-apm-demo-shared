f179b91a9fe5e20e10b0e3ab848ffc7f31a518a9
Flávio Knob <ffknob@gmail.com>
2020-07-12 20:35:37 -0300

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-/// <reference path="./types/apm/index.ts" />
-/// <reference path="./types/auth/index.ts" />
-/// <reference path="./types/backend/index.ts" />
-/// <reference path="./types/metadata/index.ts" />
-/// <reference path="./types/simulation/index.ts" />
-
-declare module '@ffknob/elastic-apm-demo-shared/types' {
-    // @ts-ignore
-    export * from '@ffknob/elastic-apm-demo-shared/types/apm';
-    // @ts-ignore
-    export * from '@ffknob/elastic-apm-demo-shared/types/auth';
-    // @ts-ignore
-    export * from '@ffknob/elastic-apm-demo-shared/types/backend';
-    // @ts-ignore
-    export * from '@ffknob/elastic-apm-demo-shared/types/metadata';
-    // @ts-ignore
-    export * from '@ffknob/elastic-apm-demo-shared/types/simulation';
-}
+// @ts-ignore
+export * from '@ffknob/elastic-apm-demo-shared/dist/types/apm';
+// @ts-ignore
+export * from '@ffknob/elastic-apm-demo-shared/dist/types/auth';
+// @ts-ignore
+export * from '@ffknob/elastic-apm-demo-shared/dist/types/backend';
+// @ts-ignore
+export * from '@ffknob/elastic-apm-demo-shared/dist/types/metadata';
+// @ts-ignore
+export * from '@ffknob/elastic-apm-demo-shared/dist/types/simulation';
