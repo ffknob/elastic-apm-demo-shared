fdbc1fa5e1f7c3413170d03d1dda48bc1c1711d6
Flávio Knob <ffknob@gmail.com>
2020-07-14 11:31:42 -0300

    Removed typeRequest

--- src/types/backend/index.ts
+++ src/types/backend/index.ts
@@ -4,4 +4,3 @@ export { BackendRequest } from './BackendRequest';
 export { BackendResponseMetadata } from './BackendResponseMetadata';
 export { BackendResponse } from './BackendResponse';
 export { BackendSuccess } from './BackendSuccess';
-export { Request } from './Request';
