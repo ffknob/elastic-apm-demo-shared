706f3559011d1b2aa61bbdb4ddfbcd26dc1b670d
Flávio Knob <ffknob@gmail.com>
2020-07-25 09:47:09 -0300

    Tried to fix span types export

--- src/types/apm/span-types.ts
+++ src/types/apm/span-types.ts
@@ -1,13 +1,11 @@
 export const SPAN_TYPES: string[] = [
-  'ext.http.http',
-  'db.elasticsearch.request',
-  'db.mongodb.query',
-  'db.postgresql.query',
-  'db.graphql.execute',
-  'cache.redis',
-  'websocket.send',
+    'ext.http.http',
+    'db.elasticsearch.request',
+    'db.mongodb.query',
+    'db.postgresql.query',
+    'db.graphql.execute',
+    'cache.redis',
+    'websocket.send'
 ];
 
-type SpanType = typeof SPAN_TYPES;
-
-export default SpanType;
+export type SpanType = typeof SPAN_TYPES;
