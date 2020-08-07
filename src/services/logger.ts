ee1c76cff382fea09b785e0a8a1f0ac77a1a4afc
Flávio Knob <ffknob@gmail.com>
2020-08-07 20:24:38 -0300

    Log level

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,7 +1,10 @@
 import winston, { Logger } from 'winston';
 
+const DEFAULT_LOG_LEVEL: string = 'error';
+
 export class LoggerService {
     static logger: Logger = winston.createLogger({
-        transports: [new winston.transports.Console()]
+        transports: [new winston.transports.Console()],
+        level: process.env.LOG_LEVEL || DEFAULT_LOG_LEVEL
     });
 }
