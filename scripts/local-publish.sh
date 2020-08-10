e7e216f6a18324ae231c000d2afdd0696695f0e4
Flávio Knob <ffknob@gmail.com>
2020-08-10 20:22:41 -0300

    Local publish script

---
+++ scripts/local-publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -R dist/* ../api/node_modules/@ffknob/elastic-apm-demo-shared/dist/
+cp -R dist/* ../app/node_modules/@ffknob/elastic-apm-demo-shared/dist/
+cp -R dist/* ../auth/node_modules/@ffknob/elastic-apm-demo-shared/dist/
+cp -R dist/* ../data/node_modules/@ffknob/elastic-apm-demo-shared/dist/
+cp -R dist/* ../simulation/node_modules/@ffknob/elastic-apm-demo-shared/dist/
